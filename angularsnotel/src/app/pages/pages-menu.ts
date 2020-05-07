c7ae6aa9efe66477a70cb232b35d5cd3e8e5f74f
mddarr <mddarr@gmail.com>
2020-05-07 12:07:07 -0700

    Angular

--- angularsnotel/src/app/pages/pages-menu.ts
+++ angularsnotel/src/app/pages/pages-menu.ts
@@ -2,15 +2,10 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'Tabs',
+    title: 'Trip Reports',
     icon: 'shopping-cart-outline',
     link: '/pages/tabs',
     home: true,
-  },
-  {
-    title: 'Upload',
-    icon: 'shopping-cart-outline',
-    link: '/pages/upload',
   },
   {
     title: 'Charts',
@@ -27,28 +22,6 @@ export const MENU_ITEMS: NbMenuItem[] = [
       {
         title: 'D3',
         link: '/pages/charts/d3',
-      },
-    ],
-  },
-  {
-    title: 'Auth',
-    icon: 'lock-outline',
-    children: [
-      {
-        title: 'Login',
-        link: '/auth/login',
-      },
-      {
-        title: 'Register',
-        link: '/auth/register',
-      },
-      {
-        title: 'Request Password',
-        link: '/auth/request-password',
-      },
-      {
-        title: 'Reset Password',
-        link: '/auth/reset-password',
       },
     ],
   },

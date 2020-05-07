0e101418e9a088361a5b40d23ed99463643bd014
mddarr <mddarr@gmail.com>
2020-05-07 14:24:40 -0700

    Install the readme, and organize the scripts a little bit more

--- scripts/install_connectors.sh
+++ scripts/install_connectors.sh
@@ -2,5 +2,5 @@
 docker exec connect confluent-hub install  debezium/debezium-connector-postgresql:0.9.4 --no-prompt
 docker restart connect
 sleep 25
-bash scripts/pipeline/post.sh elasticsearch_sink_connector.json
-bash scripts/pipeline/post.sh debezium_source_connector.json
+bash scripts/kafka/post.sh debezium_source_connector.json
+bash scripts/kafka/post.sh elasticsearch_sink_connector.json

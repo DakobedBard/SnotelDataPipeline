79745cd7143057064c9654a17f25dfb4c4999485
mddarr <mddarr@gmail.com>
2020-05-22 18:30:30 -0700

    playing w/ the vue..

--- mountainvue/src/components/LocationsList.vue
+++ mountainvue/src/components/LocationsList.vue
@@ -2,9 +2,11 @@
 
 <template>
   <div class="locations">
-    <v-simple-table height="300px">
+    
+    <v-simple-table dark>
+    
     <template v-slot:default>
-    <thead>
+    <thead style="height:1000px">
         <tr>
           <th class="text-left">Location</th>
           <th class="text-left">Elevation</th>
@@ -12,48 +14,17 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="car in cars" :key="car.name">
-          <td>{{ car }}</td>
-          <td>{{ car }}</td>
+        <tr v-for="location in locations" :key="location.location_id">
+          <td>{{ location.location_name }}</td>
+          <td> {{ location.elevation  }} </td>
           <td> Wenatchee </td>
         </tr>
       </tbody>
     </template>
     </v-simple-table>
   </div>
+
 </template>
-
-
-      <!-- <v-simple-table height="300px">
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-left">Name</th>
-          <th class="text-left">Calories</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in desserts" :key="item.name">
-          <td>{{ item.name }}</td>
-          <td>{{ item.calories }}</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table> -->
-      <!-- whad
-    <ul id="example-1">
-        <li v-for="car in cars" :key="car.name">
-            {{ car }} 
-        </li>
-    </ul>
-    <a @click="getLocations('bro')">Click me!</a>
-    <ul id="locations">
-        <li v-for="location in locations" :key="location.location_id">
-            
-            {{ location.location_name }} \n
-        </li>
-    </ul> -->
-
 
 
 
@@ -61,8 +32,45 @@
 import axios from 'axios'
 export default {
   name: 'LocationsList',
+  created(){
+    this.locationRequest()
+  },
+
   data: function() {
-		return {
+    var location_array = [];
+    axios.get('http://localhost:8081/data/location/')
+                .then(response => {location_array = response.data})
+                .catch((error) => console.log(error))
+    
+    console.log("Locations " + location_array.length)
+    
+    return {
+            search: '',
+            headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          }],
+          desserts: [
+            {
+              name: 'Frozen Yogurt',
+              calories: 159,
+              fat: 6.0,
+              carbs: 24,
+              protein: 4.0,
+              iron: '1%',
+            },
+            {
+              name: 'Ice cream sandwich',
+              calories: 237,
+              fat: 9.0,
+              carbs: 37,
+              protein: 4.3,
+              iron: '1%',
+            }],
+
             cars : ["Saab", "Volvo", "BMW"],
             attributeA: 'valueA',
             locations: []
@@ -70,11 +78,14 @@ export default {
 	}
     ,
   methods: {
-        getLocations: function (text){
-            console.log("You suck! " + text + " " + this.name )  
-            axios.get('http://localhost:8081/data/location/')
+        locationRequest: function(){
+          axios.get('http://localhost:8081/data/location/')
                 .then(response => {this.locations = response.data})
                 .catch((error) => console.log(error))
+        },
+
+        getLocations: function (text){
+            console.log("You suck! " + text + " " + this.name )  
             console.log("The number of cars is " + this.locations.length)
         }
     }
@@ -100,4 +111,6 @@ li {
 a {
   color: #42b983;
 }
+
+
 </style>
